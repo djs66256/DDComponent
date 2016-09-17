855c35927388c3ddf463c42dc8bb1d2b0b60e320
Daniel <djs66256@163.com>
2016-09-17 12:34:10 +0800

    begin change collection api

--- Class/CollectionView/CollectionViewComponent.swift
+++ Class/CollectionView/CollectionViewComponent.swift
@@ -11,27 +11,161 @@ import UIKit
 public protocol CollectionViewComponent: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     weak var superComponent: CollectionViewComponent? { get set }
     var collectionView: UICollectionView? { get }
-    var section: Int { get set }
-    var item: Int { get set }
     func prepareCollectionView()
-    func reloadIndexPath()
+//    func reloadIndexPath()
     func reloadData()
+    
+    // for subComponent
+    func firstRow(ofSubComponent: CollectionViewComponent) -> Int
+    func firstSection(ofSubComponent: CollectionViewComponent) -> Int
 }
 
-public extension CollectionViewComponent {
+//public extension CollectionViewComponent {
+//    
+//    func item(rawItem: Int) -> Int {
+//        return rawItem - self.item
+//    }
+//    func rawItem(item: Int) -> Int {
+//        return item + self.item
+//    }
+//    
+//    func section(rawSection: Int) -> Int {
+//        return rawSection - self.section
+//    }
+//    func rawSection(section: Int) -> Int {
+//        return section + self.section
+//    }
+//}
+
+open class CollectionViewBaseComponent: NSObject, CollectionViewComponent {
+    public weak var superComponent: CollectionViewComponent?
+    public var collectionView: UICollectionView? {
+        get {
+            return superComponent?.collectionView
+        }
+    }
+    open var row: Int {
+        get {
+            return superComponent?.firstRow(ofSubComponent: self) ?? 0
+        }
+    }
     
-    func item(rawItem: Int) -> Int {
-        return rawItem - self.item
-    }
-    func rawItem(item: Int) -> Int {
-        return item + self.item
+    open var section: Int {
+        get {
+            return superComponent?.firstSection(ofSubComponent: self) ?? 0
+        }
     }
     
-    func section(rawSection: Int) -> Int {
-        return rawSection - self.section
+    open func prepareCollectionView() { }
+    open func reloadData() { }
+    open func firstRow(ofSubComponent: CollectionViewComponent) -> Int {
+        return 0
     }
-    func rawSection(section: Int) -> Int {
-        return section + self.section
+    open func firstSection(ofSubComponent: CollectionViewComponent) -> Int {
+        return 0
+    }
+    
+    // MARK: DataSource
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        assert(false)
+        return UICollectionViewCell()
+    }
+    public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        return UICollectionReusableView()
+    }
+    
+    // MARK: Delegate
+    public func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) { }
+    
+    public func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) { }
+    
+    public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) { }
+    
+    public func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) { }
+    
+    
+    public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) { }
+    
+    public func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) { }
+    
+    public func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) { }
+    
+    public func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath) { }
+    
+    
+    public func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) { }
+    
+    
+    public func collectionView(_ collectionView: UICollectionView, transitionLayoutForOldLayout fromLayout: UICollectionViewLayout, newLayout toLayout: UICollectionViewLayout) -> UICollectionViewTransitionLayout {
+        return UICollectionViewTransitionLayout(currentLayout: fromLayout, nextLayout: toLayout)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.itemSize
+        }
+        return .zero
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.sectionInset
+        }
+        return .zero
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.minimumLineSpacing
+        }
+        return 0
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.minimumInteritemSpacing
+        }
+        return 0
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.headerReferenceSize
+        }
+        return .zero
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.footerReferenceSize
+        }
+        return .zero
     }
 }
 
