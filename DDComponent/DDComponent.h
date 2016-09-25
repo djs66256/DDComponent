0098f31a1004224d08b3110bbb1e0f6bc86a99f9
Daniel <djs66256@163.com>
2016-09-25 16:19:43 +0800

    for carthage

---
+++ DDComponent/DDComponent.h
@@ -0,0 +1,19 @@
+//
+//  DDComponent.h
+//  DDComponent
+//
+//  Created by daniel on 16/9/25.
+//  Copyright © 2016年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DDComponent.
+FOUNDATION_EXPORT double DDComponentVersionNumber;
+
+//! Project version string for DDComponent.
+FOUNDATION_EXPORT const unsigned char DDComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDComponent/PublicHeader.h>
+
+
